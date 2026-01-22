3855fea34f10be10663503cb82e1026a4080ad9e
Kynlow <elwen@areastudio.fr>
2026-01-22 10:14:27 +0100

    commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const $submitButton = document.querySelector("#submit-btn");
+const $dialog = document.querySelector("dialog");
+$submitButton.addEventListener("click", () => {
+    $dialog.show()
+})
